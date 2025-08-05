afc24729237c1795b270d624e5998600bb226082
ndurushima <ndu1994@gmail.com>
2025-08-05 13:52:13 -0500

    init discount to be 0

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  def __init__(self, discount):
+  def __init__(self, discount=0):
     self.discount = discount
     self.total = 0
     self.items = []
