144f7f9fd6f0320b7123ce8ce0f82b8880b86201
ndurushima <ndu1994@gmail.com>
2025-08-05 13:23:37 -0500

    Initialize Cash register. made discount a property and set it to tak int and raise an error.

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+  def __init__(self, discount):
+    self.discount = discount
+    self.total = 0
+    self.items = []
+    self.previous_transactions = []
+
+@property
+def dicount(self):
+    return self._discount
+
+@dicount.setter
+def dicount(self, value):
+    if type(value) is int and 0 <= value <= 100:
+        self._discount = value
+    else:
+        print("Not valid discount")
+
