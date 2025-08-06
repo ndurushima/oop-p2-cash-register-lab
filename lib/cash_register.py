4e26b2febc77b3c3376fa13979b639da6c8c9ddf
ndurushima <ndu1994@gmail.com>
2025-08-05 19:09:58 -0500

    tabbed the code to be in the class

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -7,39 +7,39 @@ class CashRegister:
     self.items = []
     self.previous_transactions = []
 
-@property
-def dicount(self):
-    return self._discount
+  @property
+  def dicount(self):
+      return self._discount
 
-@dicount.setter
-def dicount(self, value):
-    if type(value) is int and 0 <= value <= 100:
-        self._discount = value
-    else:
-        print("Not valid discount")
+  @dicount.setter
+  def dicount(self, value):
+      if type(value) is int and 0 <= value <= 100:
+          self._discount = value
+      else:
+          print("Not valid discount")
 
-def add_item(self, item, price, quantity):
-    new_item = {
-       'item': item,
-       'price': price,
-       'quantity': quantity
-    }
-    self.price = price * quantity
-    self.items.append(new_item)
-    self.total += self.price  
-    self.previous_transactions.append(new_item)
+  def add_item(self, item, price, quantity):
+      new_item = {
+        'item': item,
+        'price': price,
+        'quantity': quantity
+      }
+      self.price = price * quantity
+      self.items.append(new_item)
+      self.total += self.price  
+      self.previous_transactions.append(new_item)
 
-def apply_discount(self):
-    self.total -= self.total * (self.discount / 100)
-    return self.total
-    
-def void_last_transaction(self):
-    if not self.previous_transactions:
-        print("There is no transaction to void.")
-        return
-    last = self.previous_transactions.pop()
-    self.total -= last['price']
-    for _ in range(last['quantity']):
-        self.items.remove(last['item'])
-    return self.total
+  def apply_discount(self):
+      self.total -= self.total * (self.discount / 100)
+      return self.total
+      
+  def void_last_transaction(self):
+      if not self.previous_transactions:
+          print("There is no transaction to void.")
+          return
+      last = self.previous_transactions.pop()
+      self.total -= last['price']
+      for _ in range(last['quantity']):
+          self.items.remove(last['item'])
+      return self.total
 
